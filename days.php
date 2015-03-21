4840f5c31344373fd360721f4bebed3a1f27ea03
ppcinj <michael@ppcinj.com>
2015-03-21 13:25:22 +0100

    Included new days.php

--- days.php
+++ days.php
@@ -3,39 +3,52 @@
     <?php
         include './dblogin.php';
         include './globalVar.php';
+        $selectedCountry=$_GET['selectedCountry'];
+        if ($selectedCountry == -1 | !isset($_GET['selectedCountry'])) { $selectedCountry="1,2,3,4,5,6,7,8,9,10,11,12,13,14"; }		
         $cat= array();	
         $kan= array();
         $data = array();
 
-        $query2="select distinct days from import order by days desc";
-        $result2=mysql_query($query2);
+        $query_cat="select distinct days from import order by days desc";
+        $result2=mysql_query($query_cat);
         while ($line=mysql_fetch_array($result2)) {
                 $cat[]=$line[0];
         }
 
-        $query4="select import.days,sum(spend*numTrx) as total from import group by import.days order by days desc";
+        $query4="select import.days,sum(spend*numTrx) as total from import where country in (".$selectedCountry.") group by import.days order by days desc";
         $result4=mysql_query($query4);
         while ($line=mysql_fetch_array($result4)) {
                 $turnover[]=$line[1];
         }
         $turnover_sum=array_sum($turnover);
 
-        $query3="select kanton.name,kanton.fullname,sum(spend*numTrx) as total from import,locations,kanton where import.location=locations.id and locations.kt=kanton.id group by import.days,kanton.name having total > ".$turnover_sum*0.005."";
+        $query6="select merchCategory,name,sum(spend*numTrx) as total from import,merch where import.merchCategory=merch.id and country in (".$selectedCountry.") group by days,merchCategory having total > ".$turnover_sum*0.007."";
+        $result6=mysql_query($query6);
+        while ($line=mysql_fetch_array($result6)) {
+                $column[$line[0]]=array("type" => "column","name" => $line[1], "data" => array_fill(0,count($cat),0), "yAxis" => 1, "visible" => false);
+        }
+
+        $query5="select import.days,merchCategory,sum(spend*numTrx) as total from import where country in (".$selectedCountry.") group by days,merchCategory having total > ".$turnover_sum*0.007." order by days desc";
+        $result5=mysql_query($query5);
+        while ($line=mysql_fetch_array($result5)) {
+                $column[$line[1]]["data"][10-$line[0]]=$line[2];
+        }
+
+        $query3="select kanton.name,kanton.fullname,sum(spend*numTrx) as total from import,locations,kanton where import.location=locations.id and locations.kt=kanton.id and country in (".$selectedCountry.") group by import.days,kanton.name having total  > ".$turnover_sum*0.005."";
         $result3=mysql_query($query3);
         while ($line=mysql_fetch_array($result3)) {
                 $data[$line[0]]=array("name" => $line[1], "data" => array_fill(0,count($cat),0));
         }
 
-        $query="select import.days,kanton.name,sum(spend*numTrx) as total from import,locations,kanton where import.location=locations.id and locations.kt=kanton.id group by import.days,kanton.name having total  > ".$turnover_sum*0.005."";
+        $query="select import.days,kanton.name,sum(spend*numTrx) as total from import,locations,kanton where import.location=locations.id and locations.kt=kanton.id and country in (".$selectedCountry.") group by import.days,kanton.name having total  > ".$turnover_sum*0.005."";
         $result=mysql_query($query);                
         while ($line=mysql_fetch_array($result)) {
                 $data[$line[1]]["data"][10-$line[0]]=$line[2];
         }		
-        //print_r(get_defined_vars());
+
+        $column=array_values($column);
         $data=array_values($data);
-        array_push($data,array("type" => "column", "name" => "turnover", "data" => $turnover, "yAxis" => 1));
-        
-        //print_r(get_defined_vars());
+        $data=array_merge($data,$column);
     ?>
     <head>
         <meta charset="UTF-8">
@@ -56,17 +69,17 @@
         $(function () {
             $('#container').highcharts({
                 chart: {
-                    type: 'areaspline'
+                    type: 'areaspline',
+                    marginRight: 180
                 },
                 title: {
-                    text: 'days and after Lucern visit'
+                    text: 'What Lucern tourists do before they come to Lucern?'
                 },
                 legend: {
                     layout: 'vertical',
-                    align: 'left',
+                    align: 'right',
                     verticalAlign: 'top',
-                    x: 150,
-                    y: 100,
+                    y:100,
                     floating: true,
                     borderWidth: 1,
                     backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
@@ -80,11 +93,11 @@
                 yAxis: [{
                     height: 250,
                     title: {
-                        text: 'total spending'
+                        text: 'total spending / area'
                     },
                 }, {
                     title: {
-                        text: 'total spenidng'
+                        text: 'total spenidng / sector'
                     },
                     top: 320,
                     height: 130,
@@ -101,13 +114,15 @@
                 plotOptions: {
                     areaspline: {
                         fillOpacity: 0.5
-                    }
+                    },
+                    column: {
+                        stacking: 'normal'
+                    },
                 },
                 series: <?php echo json_encode($data, JSON_NUMERIC_CHECK)?>,
             });
         });
         //]]>  
-
         </script>
     </head>
     <body>
@@ -132,7 +147,7 @@
             <div class="content">
                 <form method="get" id="frmSelectCountry">
                     <span style="margin-left: 40px;">Customer Origin Country: </span><select id="soflow" onchange="document.getElementById('frmSelectCountry').submit();" name='selectedCountry'>
-                    <option value="-1">Alle</option>
+                    <option value="-1">All</option>
                     <?php
                         $res = mysql_query("select * from country");
                         while ($row = mysql_fetch_assoc($res))
@@ -145,7 +160,7 @@
                     ?>
                     </select>
                 </form>
-                <div id="container" style="min-width: 310px; height: 500px; margin: 0 auto"></div>
+                <div id="container" style="min-width: 300px; height: 505px; margin: 0 auto"></div>
             </div>
         </div>
     </body>
